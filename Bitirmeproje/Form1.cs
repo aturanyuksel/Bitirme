b031e78259210962792c527dbf9206281b1e384e
Turan Yüksel <127048706+aturanyuksel@users.noreply.github.com>
2024-03-17 11:32:59 +0300

    Visual Studio / SQLite connection
    Visual Studio / SQLite connection

---
+++ Bitirmeproje/Form1.cs
@@ -0,0 +1,38 @@
+using Microsoft.VisualBasic.ApplicationServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data.SQLite;
+
+namespace Bitirmeproje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string sqlitedb_constr = "Data source=C:\\Bitirme.db;Version=3;";
+            using(var  AC = new SQLiteConnection(sqlitedb_constr))
+            {
+                try
+                {
+                    AC.Open();
+                    MessageBox.Show("Veri tabanýna baglantý basarýyla gerceklestirildi.");
+                }
+                catch (Exception hata)
+                {
+                    MessageBox.Show("veri tabanýna gaglantý saglanamadý!","Hata Mesajý"+hata.Message);
+                    throw;
+                }
+            }
+            
+            /* string yol = Application.StartupPath;
+
+             string databaseyolu = @"Data Source =" + C:\\Users\Turann\Desktop + "\\VeritabaninizinAdi.db3; */     //veri tabanýnýn yolunu her yerde tanýmlamamýzý saðlar .
+
+
+        }
+    }
+}
