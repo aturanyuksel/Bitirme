c7247d7c4f2784112d3d171595e4e840be6e7119
Turan Yüksel <127048706+aturanyuksel@users.noreply.github.com>
2024-03-25 07:20:37 +0300

    Add files via upload

--- Bitirmeproje/Form1.Designer.cs
+++ Bitirmeproje/Form1.Designer.cs
@@ -28,19 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(1, 1);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(429, 507);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.AppWorkspace;
+            button1.Location = new Point(849, 43);
+            button1.Name = "button1";
+            button1.Size = new Size(222, 68);
+            button1.TabIndex = 1;
+            button1.Text = "Verileri Güncelle";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.AppWorkspace;
+            button2.Location = new Point(849, 132);
+            button2.Name = "button2";
+            button2.Size = new Size(222, 68);
+            button2.TabIndex = 2;
+            button2.Text = "Graik Oluştur";
+            button2.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1112, 514);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
     }
 }
--- Bitirmeproje/Form1.cs
+++ Bitirmeproje/Form1.cs
@@ -6,6 +6,8 @@ namespace Bitirmeproje
 {
     public partial class Form1 : Form
     {
+         public static SQLiteConnection connection = new SQLiteConnection("Data source=.\\Bitirme.db;Version=3;");
+
         public Form1()
         {
             InitializeComponent();
@@ -13,26 +15,33 @@ namespace Bitirmeproje
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string sqlitedb_constr = "Data source=C:\\Bitirme.db;Version=3;";
-            using(var  AC = new SQLiteConnection(sqlitedb_constr))
-            {
-                try
-                {
-                    AC.Open();
-                    MessageBox.Show("Veri tabanýna baglantý basarýyla gerceklestirildi.");
-                }
-                catch (Exception hata)
-                {
-                    MessageBox.Show("veri tabanýna gaglantý saglanamadý!","Hata Mesajý"+hata.Message);
-                    throw;
-                }
-            }
             
-            /* string yol = Application.StartupPath;
+    
+    
 
-             string databaseyolu = @"Data Source =" + C:\\Users\Turann\Desktop + "\\VeritabaninizinAdi.db3; */     //veri tabanýnýn yolunu her yerde tanýmlamamýzý saðlar .
+        /* string sqlitedb_constr = "Data source=C:\\Bitirme.db;Version=3;";
+         using(var  AC = new SQLiteConnection(connection))
+         {
+             try
+             {
+                 AC.Open();
+                 MessageBox.Show("Veri tabanýna baglantý basarýyla gerceklestirildi.");
+             }
+             catch (Exception hata)
+             {
+                 MessageBox.Show("veri tabanýna gaglantý saglanamadý!","Hata Mesajý"+hata.Message);
+                 throw;
+             }
 
 
-        }
+         }
+        
+    
+        /* string yol = Application.StartupPath;
+
+         string databaseyolu = @"Data Source =" + C:\\Users\Turann\Desktop + "\\VeritabaninizinAdi.db3; */     //veri tabanýnýn yolunu her yerde tanýmlamamýzý saðlar .
+
+
+    }
     }
 }
